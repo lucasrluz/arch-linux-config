16a83c898007478ecef7fd1ce6115a1ca2ac75ac
Lucas Ribeiro da Luz <lucasrluz.23@gmail.com>
2025-11-27 09:38:17 -0300

    feat: script para aplicar as configurações

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo pacman -S git neovim xorg xorg-xinit i3-wm i3blocks dmenu j4-dmenu-desktop alacritty fish feh chromium thunar ttf-ibm-plex gcc acpi tlp
+
+git clone https://github.com/lucasrluz/wallpaper.git
+
+mv .config/ ~/
+
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+
+sudo systemctl enable fstrim.timer
+sudo systemctl start fstrim.timer
+
+sudo systemctl enable tlp.service
+sudo systemctl start tlp.service
+sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
+
+sudo cp ./etc/tlp.conf /etc/tlp.conf
+
+sudo tlp start
+
+sudo cp ./etc/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
+
+echo "Adicionar i3 ao .xinitrc"
