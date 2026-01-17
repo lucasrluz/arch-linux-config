3529154a2426ad296144fc04a0cc01eda80a882f
Lucas Ribeiro da Luz <lucasrluz.23@gmail.com>
2026-01-16 23:21:13 -0300

    feat: adicionado arquivo de setup para o sway

---
+++ sway-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo pacman -S --needed git neovim alacritty fish chromium thunar thunar-volman gvfs thunar-archive-plugin file-roller ttf-ibm-plex ttf-ibmplex-mono-nerd gcc lxappearance network-manager-applet
+
+sudo pacman -S --needed sway wmenu
+
+git clone https://github.com/lucasrluz/dotfiles.git
+git clone https://github.com/lucasrluz/wallpaper.git
+
+mv dotfiles/.config/ ~/
+
+git config --global user.email "lucasrluz.23@gmail.com"
+git config --global user.name "Lucas Ribeiro da Luz"
+
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+# yay -S flat-remix-gtk flat-remix
